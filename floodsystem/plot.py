c2337f0124629c463e35c12bd1bc71235178be5b
OrrEos <eo379@cam.ac.uk>
2022-02-28 17:07:08 +0000

    2E typical low/high levels

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,8 +1,10 @@
+from turtle import color
 import matplotlib.pyplot as plt
 import matplotlib
 from datetime import datetime, timedelta
 from floodsystem.analysis import polyfit
 from floodsystem.stationdata import build_station_list
+from floodsystem.station import MonitoringStation
 import numpy as np
 
 def plot_water_levels(station, dates, levels):
@@ -10,10 +12,17 @@ def plot_water_levels(station, dates, levels):
     plt.plot(dates, levels)
 
     # Add axis labels, rotate date labels and add plot title
-    plt.xlabel('date')
-    plt.ylabel('water level /m')
+    plt.xlabel('Date')
+    plt.ylabel('Water Level /m')
     plt.xticks(rotation=45);
     plt.title(station)
+
+    #Typical Low and High Levels
+    lowLevel = station.typical_range[0]
+    highLevel = station.typical_range[1]
+    #plot these typical values
+    plt.plot(dates, lowLevel, color = 'b', linestyle = '-')
+    plt.plot(dates, highLevel, color = 'b', linestyle = '-')
 
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
