5cf862543c978a1498d58083523aee7ea590f6ad
Kate Lee <kl557@cam.ac.uk>
2022-03-01 02:09:21 +0000

    2F 5 output

--- Task2F.py
+++ Task2F.py
@@ -1,15 +1,54 @@
-from floodsystem.plot import plot_water_level_with_fit
+'''from floodsystem.plot import plot_water_level_with_fit
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
+import floodsystem.plot as plot'''
 
-def run():
+
+'''def run():
     stations = build_station_list()
     update_water_levels(stations)
     dt = 2
+
     print(stations[1])
     station=stations[1]
     dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-    plot_water_level_with_fit(stations[1], dates, levels,3)
+    plot.water_level_with_fit(stations[1], dates, levels,3)'''
+
+#run()
+import datetime
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+import floodsystem.plot as plot
+from floodsystem.datafetcher import fetch_measure_levels
+import matplotlib.pyplot as plt
+
+def run():
+    # Build list of stations
+    stations = build_station_list()
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    #N stations with greatest current water level
+    N =5
+    stationlist = stations_highest_rel_level(stations, N)
+    #list of names only
+    stationlistName = []
+    for i in stationlist:
+        stationlistName.append(i[0])
+  
+    dt = 2
+    for station in stations:
+        #for the top N stations, get the "whole" station info, inc measure_id
+        if station.name in stationlistName:
+            dates, levels = fetch_measure_levels(
+                station.measure_id, dt=datetime.timedelta(days=dt))
+
+            # Plot water levels against time for each station.
+            plot.plot_water_level_with_fit(station, dates, levels,4)
+            plt.tight_layout()  # so that all data labels are shown
+            #print("Plot Water Level")
+            print(station.name)
+            #plt.show()
 
 run()
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -32,20 +32,28 @@ def plot_water_levels(station, dates, levels):
     plt.show()
 
 def plot_water_level_with_fit(station, dates, levels, p):
-    poly, shift = polyfit(dates, levels, p)
-    numbered_dates = matplotlib.dates.date2num(dates)
-    dates = np.linspace(numbered_dates[0],numbered_dates[-1],50)
-    #Typical Low and High Levels
-    low_level = station.typical_range[0]
-    high_level = station.typical_range[1]
+    if len(dates) == 0 or len(levels) == 0:
+        return None
+    else:
+        poly, shift = polyfit(dates, levels, p)
+        numbered_dates = matplotlib.dates.date2num(dates)
+        #dates = np.linspace(numbered_dates[0],numbered_dates[-1],50)
+        #Typical Low and High Levels
+        low_level = station.typical_range[0]
+        high_level = station.typical_range[1]
+    
+        #trying to account for different list lengths in plotting
+        plt.axhline(y=low_level,color='r', linestyle=':')
+        plt.axhline(y=high_level, color='r',  linestyle='--')
+        #    plt.plot(dates, levels, label = 'Actual data')
+        #    plt.plot(dates, poly(dates), label = 'Polynomial model')
+        plt.plot(dates, poly(numbered_dates - shift))
 
-#trying to account for different list lengths in plotting
-    plt.axhline(y=low_level,color='r', linestyle=':')
-    plt.axhline(y=high_level, color='r',  linestyle='--')
-#    plt.plot(dates, levels, label = 'Actual data')
-#    plt.plot(dates, poly(dates), label = 'Polynomial model')
+        plt.tight_layout()  # This makes sure plot does not cut off date labels
 
-
+        plt.show()
+    
+    
 """
     for station in stations:
         if station.typical_range_consistent() != False:
