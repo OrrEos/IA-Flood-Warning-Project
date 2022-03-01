047c3e318d85a7c624c611185316a7f4cc51f5a5
Kate Lee <kl557@cam.ac.uk>
2022-03-01 10:45:19 +0000

    test 2fa

---
+++ test_analysis.py
@@ -0,0 +1,18 @@
+from floodsystem.analysis import polyfit
+from floodsystem.stationdata import build_station_list, update_water_levels
+import datetime
+from floodsystem.datafetcher import fetch_measure_levels
+import numpy as np
+
+#task 2Fa
+def test_polyfit():
+    stations = build_station_list()
+    update_water_levels(stations)
+    station = stations[0]
+    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=2))
+    poly, shift = polyfit(dates, levels, 4)
+
+    assert type(poly) == np.poly1d
+    assert shift != 0
+    assert type(shift) == np.float64
+    
