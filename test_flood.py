e29c6599e62a8cd03e2fd5e78d81f866eabf2800
Kate Lee <kl557@cam.ac.uk>
2022-03-01 10:10:09 +0000

    unit tests

--- test_flood.py
+++ test_flood.py
@@ -1,6 +1,8 @@
 from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.station import MonitoringStation
+
+#task 2b
 def test_stations_level_over_threshold():
     stations = build_station_list()
     tol = 0.8
@@ -8,7 +10,7 @@ def test_stations_level_over_threshold():
     #check that list is returned
     assert type(out) == list
     #check that items are tuples
-    #assert type(out[0]) == tuple
+    assert type(out[0]) == tuple
     #check that relative water level is a float and that station name is a string
     #for i in out:
     #    assert type(i[0]) == str
@@ -18,7 +20,7 @@ def test_stations_level_over_threshold():
     #assert len(out) >= 1
     #check that they are sorted in descending order of relative water level
 
-    
+#task 2C    
 
 def test_stations_highest_rel_level():
     stations = build_station_list()
@@ -27,7 +29,7 @@ def test_stations_highest_rel_level():
     #check that list is returned
     assert type(out) == list
     #check that list of length N
-    #assert len(out) == N
+    assert len(out) == N
 
 
     
